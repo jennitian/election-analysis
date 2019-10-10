1d46dc6cae1cc0d47c741a6986d5da9de406d46e
Jennifer Tian <jennifertian@CVRILooeyiMac02.ucsf.edu>
2019-10-09 18:25:25 -0700

    finished 3.6.1

--- pypoll.py
+++ pypoll.py
@@ -33,23 +33,32 @@ with open(file_to_load) as election_data:
             candidate_options.append(candidate_name)
             candidate_votes[candidate_name] = 0
         candidate_votes[candidate_name] += 1
-    for candidate in candidate_options:
-        votes = candidate_votes[candidate]
-        vote_percentage = (int(votes)/total_votes) * 100
-        print(f"{candidate}: {vote_percentage:.1f}% ({votes:,})\n")
+    with open(file_to_save, "w") as txt_file:
+        election_results = (
+            f"\nElection Results\n"
+            f"-----------------------------------\n"
+            f"Total Votes: {total_votes:,}\n"
+            f"-----------------------------------\n"
+        )
+        print(election_results, end="")
+        txt_file.write(election_results)
+        for candidate in candidate_options:
+            votes = candidate_votes[candidate]
+            vote_percentage = (int(votes)/total_votes) * 100
+            #print(f"{candidate}: {vote_percentage:.1f}% ({votes:,})\n")
+            
+            if (votes > winning_count) and (vote_percentage > winning_percentage):
+                winning_count = votes
+                winning_percentage = vote_percentage
+                winning_candidate = candidate
+        winning_candidate_summary = (
+            f"-----------------------------------\n"
+            f"Winner: {winning_candidate}\n"
+            f"Winning Vote Count: {winning_count}\n"
+            f"Winning Percentage: {winning_percentage:.1f}%\n"
+            f"-----------------------------------\n")
         
-        if (votes > winning_count) and (vote_percentage > winning_percentage):
-            winning_count = votes
-            winning_percentage = vote_percentage
-            winning_candidate = candidate
-    winning_candidate_summary = (
-        f"-----------------------------------\n"
-        f"Winner: {winning_candidate}\n"
-        f"Winning Vote Count: {winning_count}\n"
-        f"Winning Percentage: {winning_percentage:.1f}%\n"
-        f"-----------------------------------\n")
-    
-    print(winning_candidate_summary)
+        #print(winning_candidate_summary)
         
 
 

47241045592ac693adcad88e2bad02b36b7436d3
Jennifer Tian <jennifertian@CVRILooeyiMac02.ucsf.edu>
2019-10-09 17:32:40 -0700

    3.5.2 finished

--- pypoll.py
+++ pypoll.py
@@ -13,14 +13,25 @@ import os
 file_to_load = os.path.join("Resources", "election_results.csv")
 file_to_save = os.path.join("Analysis", "election_analysis.txt")
 
+total_votes = 0
+candidate_options = []
+
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
-    #next will skip the first row
+    
+    #the first row of headers
     headers = next(file_reader)
-    print(headers)
+    
+    for row in file_reader:
+        total_votes += 1
+        candidate_name = row[2]
+        if candidate_name not in candidate_options:
+            candidate_options.append(candidate_name)
 
-with open(file_to_save, "w") as txt_file:
-    txt_file.write("Counties in the Election\n--------------------------\nArapahoe\nDenver\nJefferson")
+print(total_votes)
+print(candidate_options)
+
+
 
 
 
